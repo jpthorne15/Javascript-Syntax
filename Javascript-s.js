208b3e5d630e380c74002362dad5df0312da9269
Jim Thorne <jamesthorne@Jamess-MacBook-Air.local>
2019-12-09 19:37:59 -0700

    first commit

---
+++ Javascript-s.js
@@ -0,0 +1,22 @@
+//Comments
+/*This is a multiline comment we can use it to 
+write long comments to help others understand our code. */
+
+//Reserved Words
+/*Words in Javascript that are defined by the language as having
+s special meaning */
+var myName = "Jim";
+let myLastName = "Thorne";
+const neverChangingData = 1989;
+
+//Brackets
+var myCars = ["Jeep Cherokee", "Chevy Silverado", "Toyota Camry"];
+
+function myOperation () {
+
+}
+
+
+//Punctuation
+'This is a string'
+"This is another string"
